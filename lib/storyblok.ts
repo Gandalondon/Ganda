5839fcf73e0bd8ba0ae70266c1efab9b00d184c6
Gandalondon <tony@gandalondon.com>
2026-06-30 18:10:40 +0100

    fix: exclude archive from work grid

--- lib/storyblok.ts
+++ lib/storyblok.ts
@@ -30,9 +30,11 @@ export async function getWorkProjects(): Promise<WorkProject[]> {
     per_page: 50,
   });
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return data.stories.map((s: any) => ({
-    slug: s.slug as string,
-    name: s.name as string,
-    thumbnail: s.content?.thumbnail?.filename as string | undefined,
-  }));
+  return data.stories
+    .filter((s: any) => s.slug !== "archive")
+    .map((s: any) => ({
+      slug: s.slug as string,
+      name: s.name as string,
+      thumbnail: s.content?.thumbnail?.filename as string | undefined,
+    }));
 }
